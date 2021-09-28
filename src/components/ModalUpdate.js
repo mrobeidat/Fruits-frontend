88eed4d8cc4037ba13258a9c9408b5b137bfbd17
Yousef Obeidat <y.linux96@gmail.com>
2021-09-28 14:31:55 +0300

    last update

--- src/components/ModalUpdate.js
+++ src/components/ModalUpdate.js
@@ -12,31 +12,31 @@ export class ModalUpdate extends Component {
                     </Modal.Header>
                     <Modal.Body>
 
-                        <Form onSubmit={(event)=>this.props.update(event)}>
+                        <Form onSubmit={(event) => this.props.update(event)}>
 
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Group className="mb-3" >
                                 <Form.Label>image</Form.Label>
-                                <Form.Control type="text" name="image" defaultValue={this.props.image}/>
+                                <Form.Control type="text" name="image" defaultValue={this.props.image} />
                             </Form.Group>
 
-                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Group className="mb-3" >
                                 <Form.Label>name</Form.Label>
                                 <Form.Control type="text" name="name" defaultValue={this.props.name} />
                             </Form.Group>
 
-                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Group className="mb-3">
                                 <Form.Label>price</Form.Label>
                                 <Form.Control type="text" name="price" defaultValue={this.props.price} />
                             </Form.Group>
 
-                            <Button variant="primary" type="submit">
-                                Save Changes
-                            </Button>
+                            <Button onClick={this.props.close}variant="primary" type="submit">
+                                submit                          
+                                  </Button>
                         </Form>
 
                     </Modal.Body>
                     <Modal.Footer>
-                      
+
                         <Button variant="primary" >
                             Save Changes
                         </Button>
